bae618b35eebb169c56d63096c618fb5712ed594
anuja-garud <anujagarud24@gmail.com>
2020-08-12 22:32:26 +0530

    Add Part time Employee & Wage

--- Employeewagescomputation.sh
+++ Employeewagescomputation.sh
@@ -1,13 +1,24 @@
 #!/bin/bash -x
-
-isPresent=1
-randomCheck=$(( RANDOM%2 ))
-
-if [ $isPresent -eq $randomCheck ]
+ispresent=1;
+randomcheck=$(( RANDOM%2 ));
+if [ $ispresent -eq $randomcheck ];
 then
-        empHours=8
-        ratePerHour=20
-        empWagesPerDay=$(( $ratePerHour*$empHours ))
+isparttime=1;
+isfulltime=2;
+emprateperhr=20;
+emphrs=8;
+salary=$(( $emphrs*$emprateperhr));
 else
-        empWagesPerDay=0
+salary=0;
 fi
+randomcheck=$(( RANDOM%3 ));
+	if [ $isparttime -eq $randomcheck ];
+		then
+			emphr=4
+		elif [ $isfulltime -eq $randomcheck ];
+		then
+			emphr=8
+		else
+			emphr=0
+	fi
+salary=$(($emphr*$emprateperhr))
